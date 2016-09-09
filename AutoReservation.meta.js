c7ddccb01450a717e925614d8c407ecc45f8483b
rtrdprgrmr <rtrdprgrmr@yahoo.co.jp>
2016-09-10 08:57:05 +0900

    test @updateURL

--- AutoReservation.meta.js
+++ AutoReservation.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version     2.07
+// @version     2.071
 // @name        Automated Keyword Reservation for www.hikaritv.net
 // @namespace   rtrdprgrmr@yahoo.co.jp
 // ==/UserScript==
