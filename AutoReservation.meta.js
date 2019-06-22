632ba06b11ebcc020d74f5b0aed99b496d87285f
rtrdprgrmr <rtrdprgrmr@yahoo.co.jp>
2019-06-22 07:21:21 +0000

    V2.36 improve auto login

--- AutoReservation.meta.js
+++ AutoReservation.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version     2.35
+// @version     2.36
 // @name        Automated Keyword Reservation for www.hikaritv.net
 // @namespace   rtrdprgrmr@yahoo.co.jp
 // ==/UserScript==
